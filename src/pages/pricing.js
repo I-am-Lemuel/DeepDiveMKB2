609302d7434c38a8c2cd5c5661d5028b5145d972
Mingus Wankum <16616@student.horizoncollege.nl>
2023-01-31 16:35:35 +0100

    added; pricing table

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -1,8 +1,25 @@
-export default function Pricing() {
+import { PricingTablePayment } from "../components/PricingTablePayment/PricingTablePayment";
+import { useState } from "react";
+import { PricingTableRefund } from "../components/PricingTableRefund/PricingTableRefund";
 
+export default function Pricing() {
+    const [isPayment, setIsPayment] = useState(true);
     return (
-        <div>
-            
+        <div className="container">
+            <div className="toggle-button d-flex justify-content-center" style={{ marginTop: "1rem" }}>
+                <button className={!isPayment ? "active btn btn-secondary me-1" : " btn btn-light me-1"} onClick={() => setIsPayment(false)}>Payment</button>
+                <button className={isPayment ? "active btn btn-secondary" : " btn btn-light"} onClick={() => setIsPayment(true)}>Refund</button>
+            </div>
+
+            <div className="toggle-content">
+                <div className={isPayment ? "d-none" : ""}>
+                    <PricingTablePayment />
+                </div>
+                <div className={!isPayment ? "d-none" : ""}>
+                    <PricingTableRefund />
+                </div>
+            </div>
         </div>
+
     )
 }
