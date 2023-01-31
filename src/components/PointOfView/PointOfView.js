ee99a650b74103f436d6a30f8789b6db6d82d3d7
Rico van Zelst <ricovzelst@pm.me>
2023-01-31 10:55:56 +0100

    home pov

---
+++ src/components/PointOfView/PointOfView.js
@@ -0,0 +1,7 @@
+export const PointOfView = () => {
+    return (
+        <div>
+            <h1>Point of View</h1>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
+import React from 'react'
+import { PointOfView } from '../components/PointOfView/PointOfView'
+
 export default function Home() {
   return (
     <div>
+      <PointOfView />
     </div>
   )
 }
