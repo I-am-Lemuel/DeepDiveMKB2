6fdb2d14a977bd78cf662ac14221ddaae293095b
Rico van Zelst <ricovzelst@pm.me>
2023-02-01 10:07:26 +0100

    fixed

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import { PricingTablePayment } from "../components/PricingTablePayment/PricingTablePayment";
 import { useState } from "react";
 import { PricingTableRefund } from "../components/PricingTableRefund/PricingTableRefund";
-=======
-
-export default function Pricing() {
->>>>>>> main
 
 export default function Pricing() {
     const [isPayment, setIsPayment] = useState(true);
@@ -24,9 +19,6 @@ export default function Pricing() {
                     <PricingTableRefund />
                 </div>
             </div>
-        <div>
-
         </div>
-
     )
 }
