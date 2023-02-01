3d64b768000bde209ab89b99c1148680a3feaba3
SudoLemuel <lembak03@hotmail.com>
2023-02-01 16:33:22 +0100

    footer icons

--- src/components/PointOfView/PointOfView.js
+++ src/components/PointOfView/PointOfView.js
@@ -13,7 +13,7 @@ export const PointOfView = () => {
                         <div className="card h-100 shadow-sm w-75 mx-auto pov-card-h">
                             <Image src="https://wsrv.nl/?url=https://www.fisdom.com/wp-content/uploads/2021/11/invoice-bill-icon.webp&w=500&h=250&fit=cover" className="card-img-top" alt="Transactions Image" />
                             <div className="card-body">
-                                <h5 className="card-title fw-semibold">Reducing transaction costs by 50%</h5>
+                                <h5 className="card-title fw-semibold">Reducing costs by 50%</h5>
                                 <p className="card-text">Due to the unique structure Globadyme adopts, we can reduce transaction costs by 50% per transaction.</p>
                             </div>
 
@@ -23,7 +23,7 @@ export const PointOfView = () => {
                         <div className="card h-100 shadow-sm w-75 mx-auto pov-card-h">
                             <Image src="https://wsrv.nl/?url=https://www.buckaroo.eu/media/3732/betaaloplossingen-buckaroo-banner-2022-2-new.png&w=500&h=250&fit=cover" className="card-img-top" alt="Payment method Image" />
                             <div className="card-body">
-                                <h5 className="card-title fw-semibold">All the payment methods & currencies you need</h5>
+                                <h5 className="card-title fw-semibold">In all the ways you need</h5>
                                 <p className="card-text">Your customers can always Pay with their preferred Payment methods and desired currencies with Globadyme.</p>
                             </div>
 
@@ -45,23 +45,27 @@ export const PointOfView = () => {
                 <div className="col-md-6">
                     <Image src="https://wsrv.nl/?url=https://i.imgur.com/oApovRS.png&w=600&h=450&fit=cover" className="card-img-top rounded rounded-4 shadow-sm " alt="Transactions Image" />
                 </div>
-                <div className="col-md-6">
-                    <h4 className="fw-semibold text-secondary">Our Vision</h4>
+                <div className="col-md-6 mt-4 d-flex row align-content-center">
+                    <h4 className="fw-semibold text-secondary">What We Do</h4>
                     <h2 className="fw-semibold">The Future of Payments has arrived</h2>
-                    <p className="mt-3 mb-3">Globadyme is a Payment Service Provider with a unique structure, that offers online companies the ability to accept payments by a variety of Payment methods and currencies. This unique structure allows us to reduce transaction costs by 50% so that our merchants do not have to pay an arm and a leg in transaction costs.</p>
-                    <p className='mb-3'>We have created a best of both worlds scenario where we can offer our merchants the combination of the best prices available in the market and an unrivalled user experience with the best onboarding process you can imagine.</p>
-                    <p >We are here to propel the payments industry forward and make it accessible for everybody, small and large. We offer complex insights into your payments data while also keeping it simple for you to understand. We offer you full transparency and make sure that you do not have to pay an arm and a leg in transaction costs.</p>
+                    <p className="mt-4">
+                        Globadyme is a PSP that offers online companys the ability to accept payments with various methods & currencies, reducing transaction costs by 50%.
+                    </p>
+                    <p className="">
+                        We offer the best prices & user experience with a simple onboarding process. We aim to propel the payments industry and provide complex insights while keeping it simple. We offer full transparency to ensure low transaction costs.
+                    </p>
                 </div>
             </div>
             {/* Call to action */}
             <div className="row mt-5 bg-secondary rounded rounded-4 shadow-sm">
                 <div className="col-md-6 ms-3">
                     <h2 className="fw-semibold text-white mt-3">Interested?</h2>
-                    <p className="text-white mt-3 mb-3">Fill out the contact form with your information and a detailed message, and we&apos;ll get back to you as soon as possible. You can also reach us by phone or email. Don&apos;t hesitate, contact us now and let us help you find the solution you need. We look forward to hearing from you!</p>
+                    <p className="text-white mt-3 mb-3">Fill out the contact form, and we&apos;ll get back to you as soon as possible.</p>
+                    <p className='text-white mt-3 mb-3'>We look forward to hearing from you!</p>
                 </div>
-                <div className="col-md-6 row justify-content-end">
+                <div className="col-md-6 row justify-content-end p-2">
                     <Link className="w-75 my-auto" href="/contact" >
-                        <button className="btn btn-light ms-5 rounded-5 w-50 me-5 fw-semibold fs-4 px-3 py-3">Contact us</button>
+                        <button className="btn btn-light ms-5 rounded-5 w-75 me-5 fw-semibold fs-4 px-3 py-3">Contact us</button>
                     </Link>
                 </div>
             </div>
--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -6,41 +6,11 @@ import { FiInstagram, FiMail } from "react-icons/fi";
 
 export const Footer = () => {
   return (
-    // <footer className="">
-    //   <ContactBtn />
-    //   <Link href="mailto:Yash.ramdjas@globadyme.com">
-    //     <FiMail size={25} color="white" />
-    //   </Link>
-    //   <Link href="https://www.instagram.com/globadyme/">
-    //     <FiInstagram size={25} color="white" />
-    //   </Link>
-    //   <Link href="https://www.linkedin.com/company/globadyme/about/">
-    //     <BsLinkedin size={25} color="white" />
-    //   </Link>
-    //   <Link href="tel:+31653795484">
-    //     <BsTelephoneFill size={25} color="white" />
-    //   </Link>
-    //   <Link href="/about">About us</Link>
-    //   <p>·</p>
-    //   <Link href="/investors">Investor Relations</Link>
-    //   <p>·</p>
-    //   <Link href="/license">Licensing</Link>
-    //   <p>·</p>
-    //   <Link href="/terms">Terms and Conditions</Link>
-    //   <p>·</p>
-    //   <Link href="/privacy">Privacy Policy</Link>
-    //   <p>·</p>
-    //   <Link href="/docs">Documentation</Link>
-    //   <div className="image">
-    //     <Image src="/logo.png" width={288} height={68} alt="company logo" />
-    //   </div>
-    //   <p className="">© 2023 Globadyme</p>
-    // </footer >
-    <footer className="d-flex">
-      <div className="col d-flex justify-content-center align-items-center">
+    <footer className="d-flex justify-content-around w-screen">
+      <div className="d-flex justify-content-center">
         <div className="">
           <Image src="/logo.png" width={288} height={68} alt="company logo" />
-          <div className="justify-content-evenly d-flex mt-2">
+          <div className="justify-content-evenly d-flex mt-4">
             <Link className="f-link" href="mailto:Yash.ramdjas@globadyme.com">
               <FiMail size={25} color="black" />
             </Link>
@@ -56,17 +26,18 @@ export const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="col d-flex justify-content-center align-items-center">
-        <div className="d-flex flex-column">
-          <h3 className="fw-bold">Company</h3>
+
+      <div className="d-flex justify-content-center">
+        <div className="d-flex flex-column mt-4">
+          <h3 className="fw-bold mb-3">Company</h3>
           <Link className="f-link" href="/about">About us</Link>
           <Link className="f-link" href="/license">Licensing</Link>
           <Link className="f-link" href="/docs">Documentation</Link>
         </div>
       </div>
-      <div className="col d-flex justify-content-center align-items-center">
-        <div className="d-flex flex-column">
-          <h3 className="fw-bold">Company</h3>
+      <div className="d-flex justify-content-center">
+        <div className="d-flex flex-column mt-4">
+          <h3 className="fw-bold mb-3">Company</h3>
           <Link className="f-link" href="/privacy">Privacy Policy</Link>
           <Link className="f-link" href="/investors">Investor Relations</Link>
           <Link className="f-link" href="/terms">Terms and Conditions</Link>
