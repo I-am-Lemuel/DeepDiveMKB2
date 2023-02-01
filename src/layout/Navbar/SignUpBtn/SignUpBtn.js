19e82d971231664248597fd869cb613c378a5ce1
SudoLemuel <lembak03@hotmail.com>
2023-02-01 16:48:56 +0100

    navbar spacing

--- src/layout/Navbar/SignUpBtn/SignUpBtn.js
+++ src/layout/Navbar/SignUpBtn/SignUpBtn.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
 export const SignUpBtn = () => {
-    return (
-        <li>
-            <Link href="/signup">
-                <button className="mx-2 btn btn-dark text-white rounded-pill shadow-sm login-btn">Sign up</button>
-            </Link>
-        </li>
-    );
-}
+  return (
+    <li>
+      <Link href="/signup">
+        <button className="mx-2 btn btn-dark text-white rounded-pill shadow-sm login-btn">
+          Sign up
+        </button>
+      </Link>
+    </li>
+  );
+};
