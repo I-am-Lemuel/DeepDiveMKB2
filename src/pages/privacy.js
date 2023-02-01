63acb1e0c65e58b8fccc4105c8179078daf03936
SudoLemuel <lembak03@hotmail.com>
2023-02-01 16:50:34 +0100

    formating pages

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,65 +1,75 @@
 export default function Privacy() {
-    return (
-        <>
-            <h1 className="display-1 fw-bold text-center">
+  return (
+    <>
+      <h1 className="display-1 fw-bold text-center">
         Privacy policy for Payment Method company
       </h1>
 
       <div className="container">
         <div className="border-bottom mb-4">
           <p className="fw-bold">
-          We understand the importance of privacy and security of our users personal and financial information. Our company 
-          sells payment methods to our customers and we are committed to protecting the privacy of our users. This Privacy Policy outlines the types of 
-          personal 
-          and financial information we collect, how we use it, and the steps we take to protect it.
+            We understand the importance of privacy and security of our users
+            personal and financial information. Our company sells payment
+            methods to our customers and we are committed to protecting the
+            privacy of our users. This Privacy Policy outlines the types of
+            personal and financial information we collect, how we use it, and
+            the steps we take to protect it.
           </p>
         </div>
         <div className="border-bottom mb-4">
           <p className="display-4 fw-normal">Types of Information Collected:</p>
           <p>
-          Personal Information: We may collect personal information such as name, address, email address, and phone number when you sign up for our services.
-            Financial Information: We collect financial information such as credit/debit card numbers, bank account numbers, 
-            and billing information in order to process payments.
-            Usage Information: We may collect information about how you use our services, such as the date and time of transactions and the type of
-             payment method used.
+            Personal Information: We may collect personal information such as
+            name, address, email address, and phone number when you sign up for
+            our services. Financial Information: We collect financial
+            information such as credit/debit card numbers, bank account numbers,
+            and billing information in order to process payments. Usage
+            Information: We may collect information about how you use our
+            services, such as the date and time of transactions and the type of
+            payment method used.
           </p>
         </div>
         <div className="border-bottom mb-4">
           <p className="display-4 fw-normal">Use of Information Collected:</p>
           <p>
-            Processing Payments: We use your financial information to process payments and provide our services.
-            Customer Support: We use your personal information to provide customer support and resolve any issues you may encounter.
-            Improving Services: We may use your usage information to improve our services and develop new ones.
-            Protection of Information:
-            We use appropriate technical and organizational measures to protect your personal 
-            and financial information. This includes secure servers, encryption, and firewalls.
+            Processing Payments: We use your financial information to process
+            payments and provide our services. Customer Support: We use your
+            personal information to provide customer support and resolve any
+            issues you may encounter. Improving Services: We may use your usage
+            information to improve our services and develop new ones. Protection
+            of Information: We use appropriate technical and organizational
+            measures to protect your personal and financial information. This
+            includes secure servers, encryption, and firewalls.
           </p>
         </div>
         <div className="border-bottom mb-4">
           <p className="display-4 fw-normal">Disclosure of Information:</p>
           <p>
             We do not sell or rent your personal information to third parties.
-            We may share your information with third parties in order to provide 
+            We may share your information with third parties in order to provide
             our services or as required by law.
           </p>
         </div>
         <div className="border-bottom mb-4">
           <p className="display-4 fw-normal">Changes to Privacy Policy:</p>
           <p>
-            We may update this Privacy Policy from time to time and will notify you
-            of any significant changes by posting a notice on our website.
+            We may update this Privacy Policy from time to time and will notify
+            you of any significant changes by posting a notice on our website.
           </p>
         </div>
         <div className="border-bottom mb-4">
           <p className="display-4 fw-normal">Contact Us:</p>
           <p>
-          If you have any questions or concerns about our
-           Privacy Policy, please contact us at [insert contact information].
+            If you have any questions or concerns about our Privacy Policy,
+            please contact us at [insert contact information].
           </p>
         </div>
-        <small>Effective Date: [Insert date of policy implementation]</small> <br />
-        <small>This Privacy Policy is governed by the laws of [insert jurisdiction].</small>
+        <small>Effective Date: [Insert date of policy implementation]</small>{" "}
+        <br />
+        <small>
+          This Privacy Policy is governed by the laws of [insert jurisdiction].
+        </small>
       </div>
-            </>
-    )
+    </>
+  );
 }
