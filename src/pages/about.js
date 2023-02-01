7ebd49edb81b7b3984b7d6af769a7365a79d4f31
Rico van Zelst <ricovzelst@pm.me>
2023-02-01 14:25:04 +0100

    🚀 add: about overview

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import { AboutOverview } from "../components/About/AboutOverview"
+
+export default function About() {
+    return (
+        <div className="container mx-auto">
+            <AboutOverview />
+        </div>
+    )
+}
