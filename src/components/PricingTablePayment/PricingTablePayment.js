c5191ce8acf65a8f26e6beae38aab962151b0630
Rico van Zelst <ricovzelst@pm.me>
2023-02-02 11:11:02 +0100

    🚀 add: contact

--- src/components/PricingTablePayment/PricingTablePayment.js
+++ src/components/PricingTablePayment/PricingTablePayment.js
@@ -1,35 +1,38 @@
+import useTranslation from "next-translate/useTranslation";
+
 export const PricingTablePayment = () => {
+  const { t } = useTranslation("pricing");
   return (
     <>
       <table className="table pricing-table">
         <thead>
           <tr>
-            <th>Payment Method</th>
-            <th>Country of client</th>
-            <th>Handling Fee Adyen</th>
+            <th>{t('payment_method')}</th>
+            <th>{t('country_of_client')}</th>
+            <th>{t('handling_fee')}</th>
             <th></th>
-            <th>Payment Method Fee</th>
+            <th>{t('payment_fee')}</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>Globcoin</td>
-            <td>Netherlands</td>
-            <td>$0.10</td>
+            <td>{t('netherlands')}</td>
+            <td>{t('currency_symbol')}0.10</td>
             <td>+</td>
             <td>5%</td>
           </tr>
           <tr>
             <td>Ideal</td>
-            <td>Worldwide</td>
-            <td>$0.10</td>
+            <td>{t('worldwide')}</td>
+            <td>{t('currency_symbol')}0.10</td>
             <td>+</td>
-            <td>v More information</td>
+            <td>10%</td>
           </tr>
           <tr>
             <td>Mastercard</td>
-            <td>Worldwide</td>
-            <td>$0.10</td>
+            <td>{t('worldwide')}</td>
+            <td>{t('currency_symbol')}0.10</td>
             <td>+</td>
             <td>2.5%</td>
           </tr>
