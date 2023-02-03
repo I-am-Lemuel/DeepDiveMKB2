521423f3d2103abdc5f4a29c419282c806f44904
SudoLemuel <lembak03@hotmail.com>
2023-02-03 09:16:31 +0100

    base method page

--- src/pages/methods/[method].js
+++ src/pages/methods/[method].js
@@ -1,4 +1,5 @@
 import useTranslation from "next-translate/useTranslation";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function Method() {
@@ -7,19 +8,22 @@ export default function Method() {
   const { t } = useTranslation("methods");
 
   return (
-    <div>
-      <div>
+    <div className="d-flex h-screen">
+      <div className="w-50">
         <button onClick={() => router.push("/products")}>Back</button>
         <h1>{t(`${method}.title`)}</h1>
         <p>{t(`${method}.description`)}</p>
       </div>
-      <div>
-        {/* <Image
-          src="/images/${method}.jpg"`}
-          width={500}
-          height={500}
-          alt={t(`${method}.title`)}
-        /> */}
+      <div className="w-50 d-flex justify-content-center">
+        <div className="w-50 bg-dark d-flex justify-content-center flex-column align-items-center">
+          <h1>{method}</h1>
+          <Image
+            src={`/images/${method}.svg`}
+            width={200}
+            height={200}
+            alt={t(`${method}.title`)}
+          />
+        </div>
       </div>
     </div>
   );
